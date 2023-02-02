b3a6f56b3786c1e93139f6ccef1a519b3ae98532
jose113df <josemateusti@gmail.com>
2023-02-02 12:04:01 -0300

    aplicativo 7

---
+++ Sa02Poo.Aplicativo7/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Sa02Poo.RegraDeNegocio;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sa02Poo.Aplicativo7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            decimal preco1, preco2, preco3, preco4, preco5;
+
+            Console.WriteLine("Digite o valor da PRIMEIRA peça: ");
+            preco1 = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor da SEGUNDA peça: ");
+            preco2 = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor da TERCEIRA peça: ");
+            preco3 = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor da QUARTA peça: ");
+            preco4 = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor da QUINTA peça: ");
+            preco5 = decimal.Parse(Console.ReadLine());
+
+            Aplicativo7RN app7 = new Aplicativo7RN();
+            Console.Clear();    
+            Console.Write("Valor total das peças: R$");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(app7.somaPecas(preco1,preco2,preco3,preco4,preco5));
+            Console.ResetColor();
+            Console.ReadLine(); 
+
+
+        }
+    }
+}
--- Sa02Poo.RegraDeNegocio/Aplicativo7RN.cs
+++ Sa02Poo.RegraDeNegocio/Aplicativo7RN.cs
@@ -6,8 +6,13 @@ using System.Threading.Tasks;
 
 namespace Sa02Poo.RegraDeNegocio
 {
-    internal class Aplicativo7RN
+    public class Aplicativo7RN
     {
+        public decimal somaPecas(decimal p1,decimal p2,decimal p3, decimal p4, decimal p5)
 
+        {
+            return p1+p2+p3+p4+p5;
+
+        }
     }
 }
