74dfc06f85f15ff797ade60dd94d319a88502e06
jose113df <josemateusti@gmail.com>
2023-02-02 11:48:32 -0300

    aplicativo 6

---
+++ Sa02Poo.Aplicativo6/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Sa02Poo.RegraDeNegocio;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sa02Poo.Aplicativo6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            decimal precoUnitario, qtdeFrota;
+
+            Console.WriteLine("digite a quantidade da sua frota: ");
+            qtdeFrota= decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o preço unitario dos Aviões");
+            precoUnitario=decimal.Parse(Console.ReadLine());
+
+            Aplicativo6RN app6= new Aplicativo6RN();
+
+            Console.Write("O preço total é ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(app6.calculo(precoUnitario, qtdeFrota));
+            Console.ResetColor();
+            Console.ReadLine();
+               
+
+
+
+        }
+    }
+}
--- Sa02Poo.RegraDeNegocio/Aplicativo6RN.cs
+++ Sa02Poo.RegraDeNegocio/Aplicativo6RN.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Sa02Poo.RegraDeNegocio
 {
-    internal class Aplicativo6RN
+    public class Aplicativo6RN
     {
+        public decimal calculo(decimal preco, decimal qtde)
+        {
+
+           return preco * qtde;
+        }
     }
 }
