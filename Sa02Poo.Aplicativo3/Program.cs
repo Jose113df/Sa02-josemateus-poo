7449ab5754b76fc38bb1f4c111ed5bddfea59780
jose113df <josemateusti@gmail.com>
2023-02-03 09:26:28 -0300

    Aplicativo 3

---
+++ Sa02Poo.Aplicativo3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Sa02Poo.RegraDeNegocio;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sa02Poo.Aplicativo3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int QtdeAvioes = 0;
+            do
+            {
+                
+                Console.WriteLine("Digite a quantidade de Aviôes: ");
+                QtdeAvioes = int.Parse(Console.ReadLine());
+                if (QtdeAvioes<=0)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Digite um numero VALIDO ");
+                
+                }
+            }
+            while (QtdeAvioes <= 0);
+
+            Aplicativo3RN app3 = new Aplicativo3RN();
+
+            Console.Write("A quantdade total de asentos é: ");
+            Console.ForegroundColor= ConsoleColor.Green;
+            Console.WriteLine(app3.MultiplicarAssentos(QtdeAvioes));
+            Console.ResetColor();
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Sa02Poo.RegraDeNegocio/Aplicativo3RN.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sa02Poo.RegraDeNegocio
+{
+    public class Aplicativo3RN
+    {
+        public int MultiplicarAssentos(int assentos)
+        {
+            int result;
+            result = assentos * 17;
+
+            return result;
+        }
+
+
+    }
+}
